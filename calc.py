80c866bc9eb146a41d9dc60cb795ef910a95c3fc
Hasan Mrisat <hasanmri890@gmail.com>
2025-06-21 03:14:52 +0300

    calc.py

--- calc.py
+++ calc.py
@@ -1,14 +1,3 @@
-
-# age = int(input("enter your age: "))
-# if age >= 100:
-#     print("you are too old to sign up")
-# elif age >= 16:
-#     print("you are eligible to sign up")
-# elif age < 0 :  
-#     print("invalid age")
-#     
-# else: print("you are NOT eligible to sign up ur age is too low")
-
 op = input("enter an op (+  -  *  /): ")
 num1 = float(input("enter first number: "))
 num2 = float(input("enter second number: "))
